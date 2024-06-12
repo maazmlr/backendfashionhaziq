58807f094ee7f2a1beab032bcacdca8c70631072
muhammad maaz <compiletimeterror@gmail.com>
2024-06-12 23:45:40 +0500

    commit

--- app.js
+++ app.js
@@ -8,17 +8,16 @@ app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-//routes
+// routes
 
-// import productRoutes from "./routes/product.routes.js";
-// import ordeRoutes from "./routes/order.routes.js";
+import productRoutes from "./routes/product.routes.js";
+import ordeRoutes from "./routes/order.routes.js";
 
-// // routes declerationOrd
+// routes declerationOrd
 
-// app.use("/api/v1/users", userRouter);
-// app.use("/api/v1/order", ordeRoutes);
-// app.use("/api/v1/product", productRoutes);
-// app.get("/", (req, res) => {
-//   return res.send("hello world");
-// });
+app.use("/api/v1/order", ordeRoutes);
+app.use("/api/v1/product", productRoutes);
+app.get("/routes", (req, res) => {
+  return res.send("hello world");
+});
 export { app };
