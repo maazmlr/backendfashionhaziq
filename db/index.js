82250cfaea0b0b4ea7a4b3020edcd76707fc664c
muhammad maaz <compiletimeterror@gmail.com>
2024-06-12 23:31:29 +0500

    snsnn

---
+++ db/index.js
@@ -0,0 +1,16 @@
+import mongoose, { connections } from "mongoose";
+import { DB_NAME } from "../constant.js";
+
+const connection = async () => {
+  try {
+    const connection = await mongoose.connect(
+      `mongodb+srv://maaz:maaz@cluster0.0zdduss.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+    );
+    console.log(connection.connection.host, "mogodb connedted");
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+export default connection;
--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import express from "express";
+import connection from "./db/index.js";
+
 const app = express();
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 8000;
+
+connection()
+  .then(
+    app.listen(PORT, () => {
+      console.log("server is running on porty ", PORT);
+    })
+  )
+  .catch((err) => console.log("connection failed", err));
